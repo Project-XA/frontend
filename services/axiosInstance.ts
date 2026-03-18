1943f10b0360d2d21d1c0f12d66a4bf4b27ade2b
Abdulrahman <abdoawwad043@gmail.com>
2026-03-18 18:02:29 +0200

    Update API Base URL

--- services/axiosInstance.ts
+++ services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'https://backend-production-9a44e.up.railway.app/api';
+const API_BASE_URL = 'https://backend-attendo.up.railway.app/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
